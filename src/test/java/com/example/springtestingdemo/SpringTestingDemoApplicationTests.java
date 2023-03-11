0e583617972c5fa809c600ba03aeb4d27c7ddf74
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-03-11 09:58:54 +0100

    initial files from spring-initializr

---
+++ src/test/java/com/example/springtestingdemo/SpringTestingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
