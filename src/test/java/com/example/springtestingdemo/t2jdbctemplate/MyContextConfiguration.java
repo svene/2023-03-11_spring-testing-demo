f444028fe103aa885a4c12bb651421821abe6400
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-03-28 21:18:52 +0200

    JdbcTemplateTest

---
+++ src/test/java/com/example/springtestingdemo/t2jdbctemplate/MyContextConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.springtestingdemo.t2jdbctemplate;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * See Baeldung article:
+ * Configuration classes annotated with @TestConfiguration are excluded from component scanning.
+ * Therefore we need to import it explicitly in every test where we want to @Autowire it.
+ * We can do that with the @Import annotation:
+ */
+@TestConfiguration
+public class MyContextConfiguration {
+	@Value("${spring.datasource.url}")
+	String jdbcUrl;
+
+	@Value("${spring.datasource.username}")
+	String username;
+
+	@Value("${spring.datasource.password}")
+	String password;
+
+	@Bean
+	public DataSource myDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(jdbcUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+
+		return dataSource;
+	}
+
+	@Bean
+	JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(myDataSource());
+	}
+
+}
---
+++ src/test/java/com/example/springtestingdemo/t2jdbctemplate/MyJdbcTemplateTest.java
@@ -0,0 +1,41 @@
+package com.example.springtestingdemo.t2jdbctemplate;
+
+import com.example.springtestingdemo.common.DbInitializer;
+import com.example.springtestingdemo.common.PostgresInitializer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+// https://www.baeldung.com/spring-jdbc-jdbctemplate
+
+// Do NOT bootstrap entire container:
+@ExtendWith(SpringExtension.class)
+@Import({MyContextConfiguration.class, DbInitializer.class})
+@ContextConfiguration(initializers = PostgresInitializer.class)
+public class MyJdbcTemplateTest {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	DbInitializer dbInitializer;
+
+
+	@Test
+	void t1() {
+		assertThat(jdbcTemplate).isNotNull();
+		assertThat(dbInitializer).isNotNull();
+	}
+
+	@Test
+	void t2() {
+		dbInitializer.initialize();
+		assertThat(jdbcTemplate.queryForObject("select count(*) from test_table", Integer.class)).isEqualTo(1);
+	}
+}
